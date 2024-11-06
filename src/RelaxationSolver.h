33eb2bb28cacb3c2b316197184b05c94db063657
nboldyrev <ya@nboldyrev.ru>
2024-11-06 18:59:56 +0300

    TODO:: define solvers

---
+++ src/RelaxationSolver.h
@@ -0,0 +1,9 @@
+#include "LinearSolver.h"
+#ifndef RELAXATIONSOLVER_H
+class RelaxtationSolver: public LinearSolver {
+    private:
+    public:
+
+};
+#define RELAXATIONSOLVER_H
+#endif //RELAXATIONSOLVER_H
---
+++ src/SeidelSolver.h
@@ -0,0 +1,9 @@
+#include "LinearSolver.h"
+#ifndef SEIDELSOLVER_H
+class SeidelSolver : public LinearSolver
+{
+    private:
+    public:
+};
+#define SEIDELSOLVER_H
+#endif //SEIDELSOLVER_H
